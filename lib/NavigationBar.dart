0e6ff6aa1ec55180b52ed588e9bcf9421cad062e
ElBatanony <ahmedelbatanony@hotmail.com>
2021-08-24 22:03:20 +0300

    Refactor NavigationBar by extracting methods

--- lib/NavigationBar.dart
+++ lib/NavigationBar.dart
@@ -15,119 +15,132 @@ class NavigationBar extends StatelessWidget {
           LogicShowCubit cubit = LogicShowCubit.get(context);
           return SafeArea(
             child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.white,
-                elevation: 2.7,
-              ),
-              drawer: Drawer(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_balance),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            "test",
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_balance),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            "test",
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_balance),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            "test",
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_balance),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            "test",
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              appBar: MyAppBar(),
+              drawer: AppDrawer(),
               body: cubit.list_widget[cubit.c_index],
-              bottomNavigationBar: BottomNavigationBar(
-                selectedItemColor: Colors.lightBlueAccent[350],
-                // selectedItemColor: cubit.list_color[cubit.c_index],
-                currentIndex: cubit.c_index,
-                onTap: (value) {
-                  cubit.getIndex(value);
-                },
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.list_outlined), label: 'list view'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.credit_card_sharp), label: 'card'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.table_chart_outlined), label: 'gallery'),
-                ],
-              ),
-              floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                  // print("${LogicShowCubit.task}");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FormScreen()),
-                  );
-                },
-                child: Icon(
-                  Icons.add,
-                  color: Colors.black,
-                ),
-                backgroundColor: Colors.grey[100],
-                elevation: 12,
-              ),
+              bottomNavigationBar: BottomNavBar(cubit),
+              floatingActionButton: FloatingActionBtn(context),
             ),
           );
         },
+      ),
+    );
+  }
+
+  AppBar MyAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 2.7,
+    );
+  }
+
+  FloatingActionButton FloatingActionBtn(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FormScreen()),
+        );
+      },
+      child: Icon(
+        Icons.add,
+        color: Colors.black,
+      ),
+      backgroundColor: Colors.grey[100],
+      elevation: 12,
+    );
+  }
+
+  BottomNavigationBar BottomNavBar(LogicShowCubit cubit) {
+    BottomNavigationBarItem BottomNavIcon(IconData iconData, String label) {
+      return BottomNavigationBarItem(icon: Icon(iconData), label: label);
+    }
+
+    return BottomNavigationBar(
+      selectedItemColor: Colors.lightBlueAccent[350],
+      currentIndex: cubit.c_index,
+      onTap: (value) => cubit.getIndex(value),
+      items: [
+        BottomNavIcon(Icons.list_outlined, 'List View'),
+        BottomNavIcon(Icons.credit_card_sharp, 'Card'),
+        BottomNavIcon(Icons.table_chart_outlined, 'Gallery')
+      ],
+    );
+  }
+
+  Drawer AppDrawer() {
+    return Drawer(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.account_balance),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  "test",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.account_balance),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  "test",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.account_balance),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  "test",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(Icons.account_balance),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  "test",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
