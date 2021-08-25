fd6d70d785b29b18707dd8fc19cb6f810787f457
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-08-25 02:13:09 +0200

    send the information to a new page (gallery view)

--- lib/gallery_screen_home.dart
+++ lib/gallery_screen_home.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_again/cubit/big_states.dart';
+import 'package:firestore_again/information_of_item_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -26,7 +27,7 @@ class galleryScreenHome extends StatelessWidget {
                 padding: EdgeInsets.all(15),
                 physics: BouncingScrollPhysics(),
                 itemBuilder: (BuildContext context, int index) =>
-                    buildTwoContainerGridView(docs[index]),
+                    buildTwoContainerGridView(docs[index],context),
                 itemCount: docs.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   mainAxisSpacing: 20,
@@ -41,39 +42,47 @@ class galleryScreenHome extends StatelessWidget {
     );
   }
 
-  Widget buildTwoContainerGridView(m) {
-    return Card(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shadowColor: Colors.blue,
-      elevation: 9,
-      margin: EdgeInsets.all(5.0),
-      child: Container(
-        alignment: Alignment.bottomCenter,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage("${m["image"]}"),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-          color: Colors.white,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Text(
-              "${m["name"]}",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  decorationStyle: TextDecorationStyle.double,
-                  textBaseline: TextBaseline.alphabetic),
-              textWidthBasis: TextWidthBasis.longestLine,
+  Widget buildTwoContainerGridView(var model,var context) {
+    return InkWell(
+      child: Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        shadowColor: Colors.blue,
+        elevation: 9,
+        margin: EdgeInsets.all(5.0),
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("${model["image"]}"),
+              fit: BoxFit.cover,
             ),
-          ],
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                "${model["name"]}",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    decorationStyle: TextDecorationStyle.double,
+                    textBaseline: TextBaseline.alphabetic),
+                textWidthBasis: TextWidthBasis.longestLine,
+              ),
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => InformationPage(model)),
+        );
+      },
     );
   }
 }
