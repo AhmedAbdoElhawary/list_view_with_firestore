c846a7f97b951f47ee5fce669b6d4835e330aa24
ElBatanony <ahmedelbatanony@hotmail.com>
2021-08-27 18:48:36 +0300

    Reformat information_of_item_page.dart

--- lib/information_of_item_page.dart
+++ lib/information_of_item_page.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 class InformationOfItemPage extends StatelessWidget {
   var model;
   String id;
-  InformationOfItemPage({required this.model,required this.id});
+  InformationOfItemPage({required this.model, required this.id});
 
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
       child: Scaffold(
         appBar: buildAppBar(),
@@ -16,8 +15,8 @@ class InformationOfItemPage extends StatelessWidget {
         floatingActionButton: buildFloatingActionButton(context),
       ),
     );
-
   }
+
   AppBar buildAppBar() {
     return AppBar(
         backgroundColor: Colors.white, iconTheme: IconThemeData.fallback());
@@ -43,9 +42,16 @@ class InformationOfItemPage extends StatelessWidget {
               ),
             ),
           ),
-          buildText(text: model["name"], fontSize: 50, fontWeight: FontWeight.bold),
-          buildText(text: model["description"], fontSize: 20, fontWeight: FontWeight.normal),
-          buildText(text: model["email"], fontSize: 15, fontWeight: FontWeight.normal),
+          buildText(
+              text: model["name"], fontSize: 50, fontWeight: FontWeight.bold),
+          buildText(
+              text: model["description"],
+              fontSize: 20,
+              fontWeight: FontWeight.normal),
+          buildText(
+              text: model["email"],
+              fontSize: 15,
+              fontWeight: FontWeight.normal),
         ],
       ),
     );
@@ -53,22 +59,31 @@ class InformationOfItemPage extends StatelessWidget {
 
   FloatingActionButton buildFloatingActionButton(BuildContext context) {
     return FloatingActionButton(
-        child: Icon(
-          Icons.edit_outlined,
-          color: Colors.black87,size: 25,
-        ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) =>new FormScreen(whichPageCome: true,model:model,id: id,)),
-          );
-        },
-        elevation: 20,
-        backgroundColor: Colors.white,
-      );
+      child: Icon(
+        Icons.edit_outlined,
+        color: Colors.black87,
+        size: 25,
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => new FormScreen(
+                    whichPageCome: true,
+                    model: model,
+                    id: id,
+                  )),
+        );
+      },
+      elevation: 20,
+      backgroundColor: Colors.white,
+    );
   }
 
-  Widget buildText({required String text, required double fontSize, required FontWeight fontWeight}) {
+  Widget buildText(
+      {required String text,
+      required double fontSize,
+      required FontWeight fontWeight}) {
     return Text(
       text,
       style: TextStyle(
